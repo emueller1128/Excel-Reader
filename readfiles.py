ee920d36ff27afd643db555a4e74102343bb3f69
Eric Mueller <50525482+emueller1128@users.noreply.github.com>
2020-01-26 15:02:58 -0600

    Added comments

--- readfiles.py
+++ readfiles.py
@@ -1,17 +1,22 @@
+#Import modules, set workspace
 import arcpy, xlrd
 arcpy.env.workspace = "C:/GIS_Programming/Final_Exam_Data"
 arcpy.env.overwriteOutput = True
 
+#Create new file geodatabase 
 gdb_path = "C:/GIS_Programming/Final_Exam_Data"
 gdb_name = "GPS_Results.gdb"
 arcpy.CreateFileGDB_management(gdb_path, gdb_name)
 
+#Create feature dataset inside newly created geodatabase
 out_name = "Study_Areas"
 out_dataset = arcpy.CreateFeatureDataset_management(gdb_name, out_name)
 
+#Set spatial reference and projection
 sr = arcpy.SpatialReference("NAD 1983 UTM Zone 12N")
 arcpy.DefineProjection_management(out_dataset, sr)
-
+ 
+#Create a shapefile of each sheet with X, Y data from an excel file
 excelfile = xlrd.open_workbook("C:/GIS_Programming/Final_Exam_Data/GPS.xlsx")
 for sheet in excelfile.sheets():
     newfc = sheet.name
